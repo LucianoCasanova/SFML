0c527bcceac1ba51b35728794bbe6b7a6e5b16e1
Luciano <lucianocasanovasanchez@gmail.com>
2024-09-05 17:42:15 -0300

    Renewable traveling circles with brightness and size effects

---
+++ configuration.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace conf
+{
+// Window configuration
+	sf::Vector2u const window_size = { 1920, 1080 };
+	sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size);
+	uint32_t const max_framerate = 144;
+	float const dt = 1.0f / static_cast<float>(max_framerate);
+
+// Star configuration
+	uint32_t const count = 1000;
+	float const radius = 20.0f;
+	float const far = 10.0f;
+	float const near = 0.0f;
+	float const speed = 1.0f;
+}
